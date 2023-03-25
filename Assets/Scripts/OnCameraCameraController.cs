06e651adcea4154f714ca987d909b48ea976e023
TheNameIsGav <spqr.dominus@gmail.com>
2023-03-25 12:28:31 -0500

    Player and Car Prefab

--- Assets/Scripts/OnCameraCameraController.cs
+++ Assets/Scripts/OnCameraCameraController.cs
@@ -6,18 +6,31 @@ using UnityEngine.InputSystem.UI;
 
 public class OnCameraCameraController : MonoBehaviour
 {
-    public Transform target;            // The target to follow
-    public float followSpeed = 5f;      // The speed at which to follow the target
-    public Vector3 offset = new Vector3(0f, 2f, -5f);  // The offset from the target's position
+    public GameObject target;    // The target object to follow
+    public Vector3 currentVectorOfMovement;
+    public Vector3 offset = new Vector3(0, 0, 0);   // The offset distance behind the target
+    public float smoothTime = 0.3f;   // The smooth time for camera movement
 
-    private Vector3 targetPosition;     // The target position to move towards
+    private Vector3 velocity = Vector3.zero;   // The current velocity of camera movement
 
-    void Update()
+    private void Start()
     {
-        // Set the target position to the target's position plus the offset
-        targetPosition = target.position + offset;
 
-        // Move the camera towards the target position with smoothing
-        transform.position = Vector3.Lerp(transform.position, targetPosition, followSpeed * Time.deltaTime);
+    }
+
+    void LateUpdate()
+    {
+
+        Debug.DrawLine(target.transform.position, target.transform.position + target.GetComponent<Rigidbody>().velocity.normalized, Color.green);
+
+        // Calculate the camera position based on the target position and offset
+        Vector3 targetPosition = target.transform.position - target.transform.forward + offset;
+        Vector3 newPosition = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+
+        // Set the camera position
+        transform.position = Vector3.Lerp(transform.position, newPosition, smoothTime);
+
+        // Rotate the camera to always face the target object
+        transform.LookAt(target.transform);
     }
 }
